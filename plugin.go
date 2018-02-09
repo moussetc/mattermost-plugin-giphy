993c69736ae56aea4901d9dd772a6be2f536de67
Clemence Mousset <clemence.mousset@outlook.fr>
2018-02-09 23:03:08 +0100

    Split GIF features from plugin and update tests

--- plugin.go
+++ plugin.go
@@ -8,15 +8,10 @@ import (
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/plugin"
 	"github.com/mattermost/mattermost-server/plugin/rpcplugin"
-	"github.com/sanzaru/go-giphy"
 )
 
-const (
-	// API key used to retrieve Giphy GIF
-	giphyAPIKey = "dc6zaTOxFJmzC"
-	// Trigger used to define the slash command
-	trigger = "gif"
-)
+// Trigger used to define the slash command
+const trigger = "gif"
 
 // GiphyPlugin is a Mattermost plugin that adds a /gif slash command
 // to display a GIF based on user keywords.
@@ -24,6 +19,8 @@ type GiphyPlugin struct {
 	api           plugin.API
 	configuration atomic.Value
 	TeamId        string
+	gifProvider   gifProvider
+	enabled       bool
 }
 
 type GiphyPluginConfiguration struct {
@@ -35,6 +32,7 @@ type GiphyPluginConfiguration struct {
 // OnActivate register the plugin command
 func (p *GiphyPlugin) OnActivate(api plugin.API) error {
 	p.api = api
+	p.enabled = true
 	err := api.RegisterCommand(&model.Command{
 		Trigger:          trigger,
 		TeamId:           p.TeamId,
@@ -63,68 +61,45 @@ func (p *GiphyPlugin) OnConfigurationChange() error {
 
 // OnDeactivate unregisters the plugin command
 func (p *GiphyPlugin) OnDeactivate() error {
+	p.enabled = false
 	return p.api.UnregisterCommand(p.TeamId, trigger)
 }
 
 // ExecuteCommand returns a post that displays a GIF choosen using Giphy
 func (p *GiphyPlugin) ExecuteCommand(args *model.CommandArgs) (*model.CommandResponse, *model.AppError) {
+	if !p.enabled {
+		return nil, appError("Cannot execute command while the plugin is disabled.", nil)
+	}
+	if p.api == nil {
+		return nil, appError("Cannot access the plugin API.", nil)
+	}
 	cmd := "/" + trigger
 	if strings.HasPrefix(args.Command, cmd) {
 		keywords := strings.Replace(args.Command, cmd, "", 1)
 
-		gifURL, err := p.getGifURL(keywords)
+		gifURL, err := p.gifProvider.getGifURL(p.config(), keywords)
 		if err != nil {
-			return nil, model.NewAppError("Giphy Plugin ExecuteCommand", "Could not get GIF", nil, "", http.StatusBadRequest)
+			return nil, appError("Unable to get GIF URL", err)
 		}
 
 		text := " *[" + keywords + "](" + gifURL + ")*\n" + "![GIF for '" + keywords + "'](" + gifURL + ")"
-		return &model.CommandResponse{ResponseType: "in_channel", Text: text, Username: args.UserId}, nil
+		return &model.CommandResponse{ResponseType: "in_channel", Text: text}, nil
 	}
 
-	return nil, model.NewAppError("Giphy Plugin ExecuteCommand", "Expected trigger "+cmd+" but got "+args.Command, nil, "", http.StatusBadRequest)
+	return nil, appError("Command trigger "+args.Command+"is not supported by this plugin.", nil)
 }
 
-// getGifURL return the URL of a small Giphy GIF that more or less correspond to requested keywords
-func (p *GiphyPlugin) getGifURL(request string) (string, error) {
-	giphy := libgiphy.NewGiphy(giphyAPIKey)
-	config := p.config()
-
-	data, err := giphy.GetTranslate(request, config.Rating, config.Language, false)
+func appError(message string, err error) *model.AppError {
+	errorMessage := ""
 	if err != nil {
-		return "", err
+		errorMessage = err.Error()
 	}
-	switch config.Rendition {
-	case "fixed_height":
-		return data.Data.Images.Fixed_height.Url, nil
-	case "fixed_height_still":
-		return data.Data.Images.Fixed_height_still.Url, nil
-	case "fixed_height_small":
-		return data.Data.Images.Fixed_height_small.Url, nil
-	case "fixed_height_small_still":
-		return data.Data.Images.Fixed_height_small_still.Url, nil
-	case "fixed_width":
-		return data.Data.Images.Fixed_width.Url, nil
-	case "fixed_width_still":
-		return data.Data.Images.Fixed_width_still.Url, nil
-	case "fixed_width_small":
-		return data.Data.Images.Fixed_width_small.Url, nil
-	case "fixed_width_small_still":
-		return data.Data.Images.Fixed_width_small_still.Url, nil
-	case "downsized":
-		return data.Data.Images.Downsized.Url, nil
-	case "downsized_large":
-		return data.Data.Images.Downsized_large.Url, nil
-	case "downsized_still":
-		return data.Data.Images.Downsized_still.Url, nil
-	case "original":
-		return data.Data.Images.Original.Url, nil
-	case "original_still":
-		return data.Data.Images.Original_still.Url, nil
-	}
-	return data.Data.Images.Fixed_height_small.Url, nil
+	return model.NewAppError("Giphy Plugin", message, nil, errorMessage, http.StatusBadRequest)
 }
 
 // Install the RCP plugin
 func main() {
-	rpcplugin.Main(&GiphyPlugin{})
+	plugin := GiphyPlugin{}
+	plugin.gifProvider = &giphyProvider{}
+	rpcplugin.Main(&plugin)
 }
