25c0a4b0483dbea7eaa3d818961abebe50fba521
moussetc <moussetc@users.noreply.github.com>
2019-06-10 00:01:04 +0200

    Add plugin name to the generated manifest

--- server/manifest.go
+++ server/manifest.go
@@ -2,8 +2,10 @@ package main
 
 var manifest = struct {
 	Id      string
+	Name    string
 	Version string
 }{
 	Id:      "com.github.moussetc.mattermost-plugin-giphy",
+	Name:    "GIF commands",
 	Version: "1.0.0",
 }
