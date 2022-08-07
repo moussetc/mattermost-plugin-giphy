f6655ee5b4d8a9ecd3d1a7409fa50f47e04aed57
moussetc <moussetc@users.noreply.github.com>
2022-08-07 17:32:05 +0200

    Bump up version to 2.1.2

--- server/manifest.go
+++ server/manifest.go
@@ -17,9 +17,9 @@ const manifestStr = `
   "description": "Add GIF slash commands from Giphy, Gfycat or Tenor",
   "homepage_url": "https://github.com/moussetc/mattermost-plugin-giphy/",
   "support_url": "https://github.com/moussetc/mattermost-plugin-giphy/issues",
-  "release_notes_url": "https://github.com/moussetc/mattermost-plugin-giphy/releases/v2.1.1",
+  "release_notes_url": "https://github.com/moussetc/mattermost-plugin-giphy/releases/v2.1.2",
   "icon_path": "assets/icon.svg",
-  "version": "2.1.1",
+  "version": "2.1.2",
   "min_server_version": "5.24.0",
   "server": {
     "executables": {
